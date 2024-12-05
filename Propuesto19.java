95e4dbe88b0f5edd022f789457ff0b9937edff0d
SamuCasta <118783859+Dagores@users.noreply.github.com>
2024-12-04 19:20:40 -0500

    Parte1

---
+++ Propuesto19.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Propuesto19 {
+    
+
+    //Dado el valor del lado en un triángulo equilátero, haga un algoritmo que obtenga el perímetro, el valor de la altura y el área del triángulo.
+    public static void main(String[] args) {
+
+        double long_lado;
+        double perimetro;
+        double altura;
+        double area;
+
+        System.out.println("Ingrese la longitud de los lados: ");
+        Scanner lados = new Scanner(System.in);
+        long_lado = lados.nextDouble();
+
+        perimetro = 3*long_lado;
+        altura = (Math.sqrt(3)*long_lado)/2;
+        area = (Math.sqrt(3)*(long_lado*long_lado))/4;
+
+        System.out.println("Perimetro: " + perimetro);
+        System.out.println("Altura: " + altura);
+        System.out.println("area: " + area);
+
+
+    }
+}
---
+++ Propuesto21.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+//Dados los tres lados de un triángulo, haga un algoritmo que encuentre: perímetro, semiperímetro y el área del triángulo.
+
+public class Propuesto21 {
+    
+    public static void main(String[] args) {
+
+        double lado1;
+        double lado2;
+        double lado3;
+        double perimetro;
+        double semiperímetro;
+        double area;
+
+        System.out.println("Ingrese la longitud del lado 1: ");
+        Scanner lados1 = new Scanner(System.in);
+        lado1 = lados1.nextDouble();
+
+        System.out.println("Ingrese la longitud del lado 2: ");
+        Scanner lados2 = new Scanner(System.in);
+        lado2 = lados2.nextDouble();
+
+        System.out.println("Ingrese la longitud del lado 3: ");
+        Scanner lados3 = new Scanner(System.in);
+        lado3 = lados3.nextDouble();
+
+        perimetro = lado1 + lado2 + lado3;
+        semiperímetro = (lado1 + lado2 + lado3)/2;
+        area = Math.sqrt(semiperímetro*(semiperímetro-lado1)*(semiperímetro-lado2)*(semiperímetro-lado3));
+
+        System.out.println("Perimetro: " + perimetro);
+        System.out.println("Semiperimetro: " + semiperímetro);
+        System.out.println("area: " + area);
+
+
+
+
+
+
+
+
+
+    }
+}
